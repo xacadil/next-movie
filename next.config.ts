f0dc63fec63dfa791287dd0737fe8084cbbf5ab1
Adil Ahmed <xac.adil@gmail.com>
2025-04-17 12:40:51 -0600

    Fixed remote path deprecation warning

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['image.tmdb.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**', // TMDb uses /t/p/ in their image paths
+      },
+    ],
   },
 };
 
